879b1c718eee37ab1b5dfdc6e636804c846d137c
Thiện <ngthien151224@gmail.com>
2025-03-10 16:23:25 +0700

    [add] lab-03 ui rsa

--- lab_01/ex02_01.py
+++ lab_01/ex02_01.py
@@ -1,4 +1,4 @@
 #Nhập tên và tuổi từ người dùng
 ten = input("Nhập tên của bạn: ")
-tuoi = input("Nhập tuổi của bạn..........: ")
+tuoi = input("Nhập tuổi của bạn: ")
 print("Chào mừng,", ten, "! Bạn", tuoi, "tuổi.")
